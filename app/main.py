89982c37ce4eb44bd167f9f984a19ea092528a31
Ilona <ilonanikolaychuk052@gmail.com>
2026-04-14 16:45:09 +0300

    fixed

--- app/main.py
+++ app/main.py
@@ -7,16 +7,6 @@ class SoftwareEngineer:
         self.skills.append(skill)
 
 
-class FrontendDeveloper(SoftwareEngineer):
-    def __init__(self, name: str) -> None:
-        super().__init__(name)
-        self.skills.extend(["JavaScript", "HTML", "CSS"])
-
-    def create_awesome_web_page(self) -> str:
-        print(f"{self.name} is creating a webpage...")
-        return "<h1>Hello world</h1>"
-
-
 class BackendDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
         super().__init__(name)
@@ -25,6 +15,16 @@ class BackendDeveloper(SoftwareEngineer):
     def create_powerful_api(self) -> str:
         print(f"{self.name} is creating an API...")
         return "http://127.0.0.1:8000"
+
+
+class FrontendDeveloper(SoftwareEngineer):
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+        self.skills.extend(["JavaScript", "HTML", "CSS"])
+
+    def create_awesome_web_page(self) -> str:
+        print(f"{self.name} is creating a webpage...")
+        return "<h1>Hello world</h1>"
 
 
 class AndroidDeveloper(SoftwareEngineer):
